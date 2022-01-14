470e510ddfd649f7857f168b05968ef875c34295
Koozu1 <onni.pylvanen@gmail.com>
2022-01-14 16:01:32 +0200

    gitignore & added files

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,42 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+    @GetMapping("/ok/{text}")
+    String ok(@PathVariable String text){
+
+        String values = text + "\n";
+        for (String item : text.split("s")){
+            values += parseVariable(item).toString();
+        }
+
+        return "to coordds :  " + values ;
+    }
+
+    public Coordinate parseVariable(String variable){
+        try {
+            String[] strItems = variable.split(",");
+            return new Coordinate(
+                    Integer.parseInt(strItems[0])
+                    , Integer.parseInt(strItems[1])
+                    , Integer.parseInt(strItems[2]));
+        }catch (Exception e){
+
+            return null;
+        }
+
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
